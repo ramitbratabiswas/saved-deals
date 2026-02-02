93e02c1777625f884e290ae9cad1d7f00c376178
Ramit Brata Biswas <ramitbratabiswas@gmail.com>
2026-02-02 01:23:56 -0800

    completed all required endpoints

--- backend/app/routes/deals.py
+++ backend/app/routes/deals.py
@@ -1,16 +1,42 @@
-from fastapi import APIRouter, Depends
-from app.schemas import DealCreate
+from fastapi import APIRouter, Depends, HTTPException
+from app.schemas import DealCreate, DealOut
+from app.models import Deal
 from app.database import get_db
 
+from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm import Session
-from sqlalchemy import select
 
-router = APIRouter(prefix="/deals")
+router = APIRouter(prefix="/deals", tags=["deals"])
 
-@router.get("/")
-def list_deals():
-    return ["bro", "haha", "u really thought"]
+@router.get("/", response_model=list[DealOut])
+def list_deals(db: Session = Depends(get_db)):
+    return db.query(Deal).all()
 
 @router.post("/")
 def create_deal(deal: DealCreate, db: Session = Depends(get_db)):
-    return deal
+    new_deal = Deal(
+        title=deal.title,
+        price=deal.price,
+        url=deal.url
+    )
+
+    db.add(new_deal)
+
+    try: 
+        db.commit()
+    except IntegrityError:
+        db.rollback()
+        raise HTTPException(status_code=409, detail="Deal with this URL already exists",)
+
+    db.refresh(new_deal)
+    return new_deal
+
+@router.delete("/{deal_id}", status_code=204)
+def delete_deal(deal_id: int, db: Session = Depends(get_db)):
+    deal = db.query(Deal).filter(Deal.id == deal_id).first()
+
+    if deal is None:
+        raise HTTPException(status_code=404, detail="Deal not found",)
+    
+    db.delete(deal)
+    db.commit()
